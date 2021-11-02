6f1130b94c8946c87212427b64f5d8d8635d5cbf
tarun-soni <soni.tarun26@gmail.com>
2021-11-02 12:45:14 +0530

    TS boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    semi: 0,
+    'react/jsx-closing-bracket-location': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-curly-new-line': 0,
+    'no-param-reassign': 0,
+  },
+}
---
+++ App/screens/Test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+// const styles = StyleSheet.create({})
+
+const Test = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  )
+}
+
+export default Test
