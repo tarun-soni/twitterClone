83b39b4c9eff4ffad383e174eb14bb307face0ed
tarun-soni <soni.tarun26@gmail.com>
2021-11-10 19:15:39 +0530

    updated aws package and singup user

--- App/index.tsx
+++ App/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import 'react-native-gesture-handler'
+import React, { useEffect } from 'react'
 import { StatusBar } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import Amplify from 'aws-amplify'
+import Amplify, { Auth } from 'aws-amplify'
 import useColorScheme from './hooks/useColorScheme'
 import Navigation from './navigation'
 import config from '../src/aws-exports'
-//@ts-ignore
-// import { withAuthenticator } from 'aws-amplify-react-native'
+// @ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native'
 
 Amplify.configure(config)
 const App = () => {
   const colorScheme = useColorScheme()
+  useEffect(() => {
+    async function run() {
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      })
+      console.log('userInfo :>> ', userInfo)
+    }
+    run()
+  }, [])
   return (
     <SafeAreaProvider>
       <StatusBar />
@@ -19,5 +29,4 @@ const App = () => {
   )
 }
 
-// export default withAuthenticator(App)
-export default App
+export default withAuthenticator(App)
